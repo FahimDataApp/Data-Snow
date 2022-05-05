769eda3f202b3fd7f0c025e45c77cf3e5c39be40
Fahim Golandaj <104915766+FahimDataApp@users.noreply.github.com>
2022-05-05 14:09:32 +0530

    Update streamlit_app.py
    Added header and some text lines.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,8 @@
 import streamlit
 
 streamlit.title('My parents new health Diner')
+
+streamlit.header('Breakfat Menu')
+streamlit.text('Omega & Blueberry OatMeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Bolid High-Range Eggs')
