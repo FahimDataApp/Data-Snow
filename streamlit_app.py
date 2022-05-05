28c7bd408dfb330b12c3c0d84f6ce13b6be97ca4
Fahim Golandaj <104915766+FahimDataApp@users.noreply.github.com>
2022-05-05 18:01:02 +0530

    Update streamlit_app.py
    added fruit_selected and fruit_to_show. this show display the fruits selected in picklist

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Lets put a pick list here so that they can pick any fruit they want from fruit list
-streamlit.multiselect ("Pick Some Fruits : ", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruit_selected = streamlit.multiselect ("Pick Some Fruits : ", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruit_to_show = my_fruit_list.loc(fruit_selected)
 
 #Display the table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruit_to_show)
