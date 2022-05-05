11e8eca8cbad538e505a81a82d379a20b34cea61
Fahim Golandaj <104915766+FahimDataApp@users.noreply.github.com>
2022-05-05 14:10:14 +0530

    Update streamlit_app.py
    Updated typo on Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 
 streamlit.title('My parents new health Diner')
 
-streamlit.header('Breakfat Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega & Blueberry OatMeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
 streamlit.text('Hard-Bolid High-Range Eggs')
