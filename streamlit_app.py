92c13bea38aecbcffceab242e31efb570db89796
Fahim Golandaj <104915766+FahimDataApp@users.noreply.github.com>
2022-05-05 14:24:44 +0530

    Update streamlit_app.py
    Added food icons to text line

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit
 
 streamlit.title('My Mom\'s New Health Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega & Blueberry OatMeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-Bolid High-Range Eggs')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
