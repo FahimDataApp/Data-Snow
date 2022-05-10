3a120710912d2c3f63ab74a91ae69fcbf764def2
Fahim Golandaj <104915766+FahimDataApp@users.noreply.github.com>
2022-05-10 17:08:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 
 streamlit.title('My Mom\'s New Health Diner')
 
-
+streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
